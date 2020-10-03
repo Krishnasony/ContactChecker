2ca6bdcac0a0dd2c9086ce75ee4e9df3774bd01c
Krishnasony <krishnasony97@gmail.com>
2020-10-03 20:29:25 +0530

    Some changes

--- app/src/main/java/com/example/contactchecker/receiver/CallReceiver.kt
+++ app/src/main/java/com/example/contactchecker/receiver/CallReceiver.kt
@@ -11,20 +11,26 @@ import java.util.*
 import kotlin.system.exitProcess
 
 class CallReceiver : PhoneCallReceiver() {
-    var context: Context? = null
+
     override fun onIncomingCallStarted(ctx: Context?, number: String?, start: Date?) {
         Toast.makeText(ctx, "Incoming Call from $number", Toast.LENGTH_LONG).show()
-        context = ctx
+        showCallerDialog(ctx, number)
+    }
+
+    override fun onIncomingCallEnded(ctx: Context?, number: String?, start: Date?, end: Date?) {
+    }
+
+    override fun onOutgoingCallStarted(ctx: Context?, number: String?) {
+        showCallerDialog(ctx, number)
+    }
+
+    private fun showCallerDialog(context: Context?, number: String?) {
         val intent = Intent(context, PhoneCallDialog::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
         intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
         intent.putExtra(INTENT_KEY_PHONE, number)
         Handler(Looper.getMainLooper()).postDelayed({ context?.startActivity(intent) }, 2000)
-    }
-
-    override fun onIncomingCallEnded(ctx: Context?, number: String?, start: Date?, end: Date?) {
-        Toast.makeText(ctx, "Call Ended $number", Toast.LENGTH_LONG).show()
     }
 
 }
--- app/src/main/java/com/example/contactchecker/receiver/PhoneCallReceiver.kt
+++ app/src/main/java/com/example/contactchecker/receiver/PhoneCallReceiver.kt
@@ -6,35 +6,46 @@ import android.content.Intent
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
 import android.util.Log
+import android.widget.Toast
 import java.util.*
 
 
 abstract class PhoneCallReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent) {
-        val teleMgr = context?.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        val psl: PhoneStateListener = object : PhoneStateListener() {
-            override fun onCallStateChanged(state: Int, incomingNumber: String) {
-                when (state) {
-                    TelephonyManager.CALL_STATE_RINGING -> {
-                        Log.i("CallReceiverBroadcast", "Incoming call caught")
-                        onIncomingCallStarted(context, incomingNumber, Date())
-                    }
-                    TelephonyManager.CALL_STATE_IDLE -> {
-                        Log.i("CallReceiverBroadcast", "CALL_STATE_IDLE")
-                        onIncomingCallEnded(context, incomingNumber, Date(), Date())
-                    }
-                    TelephonyManager.CALL_STATE_OFFHOOK -> {
-                        Log.i("CallReceiverBroadcast", "CALL_STATE_OFFHOOK")
-                        onIncomingCallEnded(context, incomingNumber, Date(), Date())
+        if (intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+                .equals(TelephonyManager.EXTRA_STATE_OFFHOOK, true)
+            || intent.action.equals("android.intent.action.NEW_OUTGOING_CALL")
+        ) {
+            val number = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
+            Toast.makeText(context, "Outgoing Call from $number", Toast.LENGTH_LONG).show()
+            onOutgoingCallStarted(context, number)
+        } else {
+            val teleMgr = context?.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val psl: PhoneStateListener = object : PhoneStateListener() {
+                override fun onCallStateChanged(state: Int, incomingNumber: String) {
+                    when (state) {
+                        TelephonyManager.CALL_STATE_RINGING -> {
+                            Log.i("CallReceiverBroadcast", "Incoming call caught")
+                            onIncomingCallStarted(context, incomingNumber, Date())
+                        }
+                        TelephonyManager.CALL_STATE_IDLE -> {
+                            Log.i("CallReceiverBroadcast", "CALL_STATE_IDLE")
+                            onIncomingCallEnded(context, incomingNumber, Date(), Date())
+                        }
+                        TelephonyManager.CALL_STATE_OFFHOOK -> {
+                            Log.i("CallReceiverBroadcast", "CALL_STATE_OFFHOOK")
+                            onIncomingCallEnded(context, incomingNumber, Date(), Date())
+                        }
                     }
                 }
             }
+            teleMgr.listen(psl, PhoneStateListener.LISTEN_CALL_STATE)
+            teleMgr.listen(psl, PhoneStateListener.LISTEN_NONE)
         }
-        teleMgr.listen(psl, PhoneStateListener.LISTEN_CALL_STATE)
-        teleMgr.listen(psl, PhoneStateListener.LISTEN_NONE)
     }
 
     abstract fun onIncomingCallStarted(ctx: Context?, number: String?, start: Date?)
     abstract fun onIncomingCallEnded(ctx: Context?, number: String?, start: Date?, end: Date?)
+    abstract fun onOutgoingCallStarted(ctx: Context?, number: String?)
 }
